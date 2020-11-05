a08af4ea29049c758176acf76935d848e49f24cc
yhkim <yhkim@hunesion.com>
2020-11-05 14:22:49 +0900

    Customer props 전달

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'name': '송예원',
+  'birthday': '123456',
+  'gender': '여자',
+  'job': 'wife'
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    )
+  }  
 }
 
 export default App;
