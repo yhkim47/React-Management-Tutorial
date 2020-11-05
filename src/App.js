8bfec1af06063f69ea035726e3de81f3c272310b
yhkim <yhkim@hunesion.com>
2020-11-05 17:18:41 +0900

    CustomerProfile, CustomerInfo 분리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Component } from 'react';
 import Customer from './components/Customer';
 
 const customer = {
+  'id': 'yhkim47',
+  'image': 'https://placeimg.com/64/64/any',
   'name': '송예원',
   'birthday': '123456',
   'gender': '여자',
@@ -14,6 +16,8 @@ class App extends Component {
   render() {
     return (
       <Customer
+        id={customer.id}
+        image={customer.image}
         name={customer.name}
         birthday={customer.birthday}
         gender={customer.gender}
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,10 +4,40 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
+                <CustomerProfile 
+                    id={this.props.id}
+                    name={this.props.name}
+                    image={this.props.image}
+                />
+                <CustomerInfo 
+                    birthday={this.props.birthday}
+                    gender={this.props.gender}
+                    job={this.props.job}
+                />
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return(
+            <div>
+                <img src={this.props.image} alt='profile' />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+
+class CustomerInfo extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>{this.props.birthday}</h2>
+                <h2>{this.props.gender}</h2>
+                <h2>{this.props.job}</h2>
             </div>
         )
     }
