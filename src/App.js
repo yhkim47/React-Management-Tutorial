c70f4bb3a60ec91f36d0e1e304107270f6837af3
yhkim <yhkim@hunesion.com>
2020-11-05 17:37:51 +0900

    custemers.map 사용

--- src/App.js
+++ src/App.js
@@ -3,26 +3,52 @@ import './App.css';
 import { Component } from 'react';
 import Customer from './components/Customer';
 
-const customer = {
-  'id': 'yhkim47',
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '송예원',
-  'birthday': '123456',
-  'gender': '여자',
-  'job': 'wife'
-};
+const customers = [
+  {
+    'id': 'yhkim47',
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '김영환',
+    'birthday': '123456',
+    'gender': '남자',
+    'job': 'developer'
+  },
+  {
+      'id': 'songkn89',
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '송금년',
+      'birthday': '123456',
+      'gender': '여자',
+      'job': 'wife'
+  },
+  {
+    'id': 'songyw89',
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '송예원',
+    'birthday': '123456',
+    'gender': '여자',
+    'job': 'wife'
+  }              
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(customer => {
+            return (
+              <Customer
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            )
+          })
+        }
+      </div>
     )
   }  
 }
